27e25173af70a9e77d7b962242982db146a66c78
Andri Thorlacius <andri.thorlacius@gmail.com>
2017-01-07 14:03:26 +0000

    Extracted waypoint class to a seperate file

--- BiPolarTowerDefence/Entities/Level.cs
+++ BiPolarTowerDefence/Entities/Level.cs
@@ -51,24 +51,4 @@ namespace BiPolarTowerDefence.Entities
             this.Waypoints.Add(new Waypoint(X,Y));
         }
     }
-
-    internal class Waypoint
-    {
-        public Vector3 position;
-
-        public Waypoint(Vector3 position)
-        {
-            this.position = position;
-        }
-
-        public Waypoint(float X, float Z)
-        {
-            this.position = new Vector3(X,0,Z);
-        }
-
-        public Waypoint(float X, float Y, float Z)
-        {
-            this.position = new Vector3(X,Y,Z);
-        }
-    }
 }
---
+++ BiPolarTowerDefence/Entities/Waypoint.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace BiPolarTowerDefence.Entities
+{
+    internal class Waypoint
+    {
+        public Vector3 position;
+
+        public Waypoint(Vector3 position)
+        {
+            this.position = position;
+        }
+
+        public Waypoint(float X, float Z)
+        {
+            this.position = new Vector3(X,0,Z);
+        }
+
+        public Waypoint(float X, float Y, float Z)
+        {
+            this.position = new Vector3(X,Y,Z);
+        }
+    }
+}
