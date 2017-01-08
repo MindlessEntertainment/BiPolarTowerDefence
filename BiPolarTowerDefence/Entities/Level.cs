4eb65cb66c57ba33042fad15c619b60f6d9b5a86
Andri Thorlacius <andri.thorlacius@gmail.com>
2017-01-08 16:13:53 +0000

    Added the turn animation to the towers

--- BiPolarTowerDefence/Entities/Level.cs
+++ BiPolarTowerDefence/Entities/Level.cs
@@ -204,7 +204,7 @@ namespace BiPolarTowerDefence.Entities
             AddComponent(new Tower(this, new Vector3(3 * Tile.TILE_SIZE + Tile.TILE_SIZE, 0, 5 * Tile.TILE_SIZE + Tile.TILE_SIZE)));
             AddComponent(new Tower(this, new Vector3(3 * Tile.TILE_SIZE + Tile.TILE_SIZE, 0, 6 * Tile.TILE_SIZE + Tile.TILE_SIZE)));
             AddComponent(new Tower(this, new Vector3(3 * Tile.TILE_SIZE + Tile.TILE_SIZE, 0, 7 * Tile.TILE_SIZE + Tile.TILE_SIZE)));
-            AddComponent(new Tower(this, new Vector3(10* Tile.TILE_SIZE + Tile.TILE_SIZE, 0, 3 * Tile.TILE_SIZE + Tile.TILE_SIZE)));
+            AddComponent(new Tower(this, new Vector3(15* Tile.TILE_SIZE + Tile.TILE_SIZE, 0, 10 * Tile.TILE_SIZE + Tile.TILE_SIZE)));
         }
     }
 }
